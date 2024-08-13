99cc4e2e7b43036ee7713d1a19f1488e9e9e99b2
Siddharth Saha <siddharth.saha@cred.club>
2024-08-13 17:16:02 +0530

    some progress in data helpers

--- code/house_prices/data_helpers.py
+++ code/house_prices/data_helpers.py
@@ -1,2 +1,31 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+import torch
+from torch import Tensor
 
+
+class DataHelpers:
+
+    @staticmethod
+    def _categorical_cols() -> list:
+        return [
+            'MSSubClass',
+            'MSZoning',
+        ]
+    
+    @staticmethod
+    def _numerical_cols() -> list:
+        return [
+            'LotFrontage',
+        ]
+
+    @classmethod
+    def make_data(cls, csv_filepath: str) -> Tensor:
+        df: pd.DataFrame = pd.read_csv(csv_filepath)
+        if df.empty:
+            return None
+        
+        categorical_cols: list = cls._categorical_cols()
+        numerical_cols: list = cls._numerical_cols()
+        
+        return df
