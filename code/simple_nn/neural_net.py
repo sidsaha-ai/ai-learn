d715ca4fc3e1fbb688dde9f3b194893e656ac530
Siddharth Saha <siddharth.saha@cred.club>
2024-08-13 12:47:42 +0530

    first cut of the neural network

---
+++ code/simple_nn/neural_net.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class SimpleNeuralNet(nn.Module):
+    """
+    Class that defines the neural network architecture. This class defines the layers and
+    how to do the forward pass through the neural network.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        # define the layers
+        self.layer1 = nn.Linear(3, 6)  # input layer takes 3 inputs and feeds 6 outputs to the next layer
+        self.layer2 = nn.Linear(6, 4)  # 6 neurons feeds to 4 neurons in the next layer
+        self.layer3 = nn.Linear(4, 2)  # 4 neurons feeds to 2 neurons in the next layer
+        self.layer4 = nn.Linear(2, 1)  # output layer takes 2 inputs and outputs 1 output.
+
+        self.activation = nn.Tanh()  # the activation function
+
+    def forward(self, x):
+        """
+        Defines how to do the forward pass through the network.
+        """
+        x = self.activation(self.layer1(x))
+        x = self.activation(self.layer2(x))
+        x = self.activation(self.layer3(x))
+        x = self.layer4(x)
+        return x
