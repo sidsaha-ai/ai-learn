2c368c11fc8c2870695854352e08d86a6b945bb9
Siddharth Saha <siddharth.saha@cred.club>
2024-08-14 18:01:41 +0530

    make the engine as a class

--- code/house_prices/main.py
+++ code/house_prices/main.py
@@ -9,39 +9,47 @@ from house_prices.neural_net import HousePricesNN
 from torch import Tensor, nn, optim
 
 
-def train(num_epochs: int, train_data_file: str) -> None:
-    """
-    Function that trains the neural network.
-    """
-    # make training data
-    data_helpers = DataHelpers()
+class MainEngine:
 
-    inputs: Tensor
-    targets: Tensor
-    inputs, targets = data_helpers.make_data(train_data_file)
-    print(f'{inputs.size()}')
-    print(f'{targets.size()}')
+    def __init__(self, train_data_file: str, test_data_file: str, num_epochs: int) -> None:
+        self.data_helpers: DataHelpers = DataHelpers()
+        self.model: HousePricesNN = None
 
-    model = HousePricesNN(
-        input_dimension=inputs.shape[1],
-        output_dimension=targets.shape[1],
-    )
-    loss_fn = nn.MSELoss(reduction='sum')
-    learning_rate: float = 1e-4
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        self.train_data_file: str = train_data_file
+        self.test_data_file: str = test_data_file
+        self.num_epochs = num_epochs
 
-    for epoch in range(num_epochs):
-        optimizer.zero_grad()
-        outputs: Tensor = model(inputs)
-        loss: Tensor = loss_fn(outputs, targets)
+    def train(self) -> None:
+        """
+        Function that trains the neural network.
+        """
+        inputs: Tensor
+        targets: Tensor
+        inputs, targets = self.data_helpers.make_data(self.train_data_file)
+        print(f'{inputs.size()}')
+        print(f'{targets.size()}')
 
-        loss.backward()
-        optimizer.step()
+        self.model = HousePricesNN(
+            input_dimension=inputs.shape[1],
+            output_dimension=targets.shape[1],
+        )
+        loss_fn = nn.MSELoss(reduction='sum')
+        learning_rate: float = 1e-4
+        optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
 
-        if (epoch + 1) % 100 == 0:
-            utils.print_loss(epoch, loss)
+        for epoch in range(self.num_epochs):
+            optimizer.zero_grad()
+            outputs: Tensor = self.model(inputs)
+            loss: Tensor = loss_fn(outputs, targets)
 
-    utils.print_loss('FINAL', loss)
+            loss.backward()
+            optimizer.step()
+
+            if (epoch + 1) % 100 == 0:
+                utils.print_loss(epoch, loss)
+
+        utils.print_loss('FINAL', loss)
+
 
 
 if __name__ == '__main__':
@@ -52,10 +60,14 @@ if __name__ == '__main__':
     parser.add_argument(
         '--train_data_file', required=True, type=str,
     )
+    parser.add_argument(
+        '--test_data_file', required=False, type=str,  # TODO: change to required
+    )
 
     args = parser.parse_args()
 
-    train(
-        args.num_epochs,
-        args.train_data_file,
+    engine = MainEngine(
+        args.train_data_file, args.test_data_file, args.num_epochs,
     )
+    
+    engine.train()
