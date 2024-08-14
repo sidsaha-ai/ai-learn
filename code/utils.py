c5d22a55a1e3251d2d55d398a9dd7696b63328bd
Siddharth Saha <siddharth.saha@cred.club>
2024-08-14 12:35:58 +0530

    fix lint

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,11 @@
+"""
+This file contains various utility functions.
+"""
 from torch import Tensor
 
 
 def print_loss(epoch: int, loss: Tensor) -> None:
+    """
+    Function to print loss for an epoch.
+    """
     print(f'{epoch=}, loss={loss.item():.4f}')
