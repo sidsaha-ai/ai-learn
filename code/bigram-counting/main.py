92fbfb978d9d01f337a205ec59e22990648c7239
Siddharth Saha <siddharth.saha@cred.club>
2024-08-22 13:28:21 +0530

    read training dataset

--- code/bigram-counting/main.py
+++ code/bigram-counting/main.py
@@ -1,10 +1,29 @@
 import pandas as pd
+import argparse
 
 
-def main():
-    # read the words from the training data
-    print('in the main function')
+def _read_words(filepath: str) -> list:
+    # reads the file to load all the words in the train dataset.
+    words: list = open(filepath).read().splitlines()
+    return words
+
+
+def main(
+    train_data_filepath: str,
+) -> None:
+    words: list = _read_words(train_data_filepath)
+    print(words[0:10])
+    print(len(words))
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train_data_filepath', required=True, type=str,
+    )
+    
+    args = parser.parse_args()
+
+    main(
+        args.train_data_filepath,
+    )
