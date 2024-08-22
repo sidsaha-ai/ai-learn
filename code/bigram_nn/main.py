fa06baf4923d929cc47e34b47e079659582fce54
Siddharth Saha <siddharth.saha@cred.club>
2024-08-22 17:50:07 +0530

    start work on bigram neural network

---
+++ code/bigram_nn/main.py
@@ -0,0 +1,28 @@
+import argparse
+
+def _read_words(filepath: str) -> list:
+    words: list = []
+
+    with open(filepath, encoding='utf-8') as f:
+        words = f.read().splitlines()
+    
+    return [w.lower() for w in words]
+
+def main(train_data_filepath: str) -> None:
+    words: list = _read_words(train_data_filepath)
+
+    print(words)
+    print(f'{len(words)=}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train_data_filepath', required=True, type=str,
+    )
+
+    args = parser.parse_args()
+
+    main(
+        args.train_data_filepath,
+    )
