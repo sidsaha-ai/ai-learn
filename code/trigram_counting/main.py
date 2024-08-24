be28ee9a0058ee3895058977efef79cdcef05d28
Siddharth Saha <siddharth.saha@cred.club>
2024-08-24 10:57:11 +0530

    start building the trigram model with counting

---
+++ code/trigram_counting/main.py
@@ -0,0 +1,28 @@
+import argparse
+
+
+def _read_words(filepath: str) -> list:
+    words: list = []
+    with open(filepath, mode='r', encoding='utf-8') as f:
+        words = f.read().splitlines()
+    
+    words = [w.lower().strip() for w in words]
+    return words
+
+
+def main(train_data_filepath: str) -> None:
+    words = _read_words(train_data_filepath)
+    print(words)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train_data_filepath', required=True, type=str,
+    )
+    
+    args = parser.parse_args()
+
+    main(
+        args.train_data_filepath,
+    )
