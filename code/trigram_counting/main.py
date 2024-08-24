af62481c61bb205d69df9e27e9ca4a085f7c9ccc
Siddharth Saha <siddharth.saha@cred.club>
2024-08-24 10:58:43 +0530

    fix lint

--- code/trigram_counting/main.py
+++ code/trigram_counting/main.py
@@ -1,3 +1,6 @@
+"""
+The main file which will start the execution.
+"""
 import argparse
 
 
@@ -5,12 +8,15 @@ def _read_words(filepath: str) -> list:
     words: list = []
     with open(filepath, mode='r', encoding='utf-8') as f:
         words = f.read().splitlines()
-    
+
     words = [w.lower().strip() for w in words]
     return words
 
 
 def main(train_data_filepath: str) -> None:
+    """
+    The main executable function.
+    """
     words = _read_words(train_data_filepath)
     print(words)
 
@@ -20,7 +26,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--train_data_filepath', required=True, type=str,
     )
-    
+
     args = parser.parse_args()
 
     main(
