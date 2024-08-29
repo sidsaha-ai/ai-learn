e6c5d61d8c1960ed5764c74b80a58cca04ca2736
Siddharth Saha <siddharth.saha@cred.club>
2024-08-29 12:02:22 +0530

    some progress in model training

--- code/trigram_nn/model.py
+++ code/trigram_nn/model.py
@@ -5,6 +5,7 @@ This file implements the trigram model using neural network.
 import string
 import torch
 from torch import Tensor
+from torch.nn import functional as F
 
 
 class TrigramNN:
@@ -16,6 +17,10 @@ class TrigramNN:
         self.ltoi: dict = {}
         self.itol: dict = {}
         self._make_char_int_mappings()
+
+        # the inputs and targets tensor for training the neural network
+        self.inputs: Tensor = None
+        self.targets: Tensor = None
     
     def _make_char_int_mappings(self) -> None:
         letters: list = ['.'] + list(string.ascii_lowercase)
@@ -24,31 +29,41 @@ class TrigramNN:
             self.ltoi[l] = ix
             self.itol[ix] = l
     
-    def _make_trigrams(self) -> tuple[list, list]:
+    def _make_training_data(self) -> None:
         """
         Make trigrams with 2 input letters and 1 target letter.
         """
-        inputs: list = []  # 2-letter inputs
-        targets: list = []  # corresponding outputs
+        l1_inputs: list = []  # first letter input
+        l2_inputs: list = []  # second letter input
+        targets: list = []  # corresponding targets
 
         for word in self.input_words:
             word = f'..{word}.'
             
             for l1, l2, l3 in zip(word, word[1:], word[2:]):
-                inputs.append(f'{l1}{l2}')
-                targets.append(l3)
+                l1_inputs.append(self.ltoi.get(l1))
+                l2_inputs.append(self.ltoi.get(l2))
+                targets.append(self.ltoi.get(l3))
         
-        return inputs, targets
+        # make targets as one-hot encoded tensor
+        self.targets = F.one_hot(
+            torch.tensor(targets), num_classes=len(self.ltoi),
+        )
+
+        # make l1_input and l2_input as one hot encoded tensors
+        t_l1_inputs: Tensor = F.one_hot(
+            torch.tensor(l1_inputs), num_classes=len(self.ltoi),
+        )
+        t_l2_inputs: Tensor = F.one_hot(
+            torch.tensor(l2_inputs), num_classes=len(self.ltoi),
+        )
+        # concat both the tensor to make the input tensor
+        self.inputs = torch.cat(
+            (t_l1_inputs, t_l2_inputs), dim=1,
+        )
 
     def train(self) -> None:
         """
         This method trains the neural network.
         """
-        inputs, targets = self._make_trigrams()
-
-        ix = 0
-        for input, target in zip(inputs, targets):
-            ix += 1
-            if ix >= 10:
-                break
-            print(f'{input} : {target}')
+        self._make_training_data()
