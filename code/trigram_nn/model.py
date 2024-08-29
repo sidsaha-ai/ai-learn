e56b29902e0dd8aa7d55ccf3c9eee9fb23f2e6b3
Siddharth Saha <siddharth.saha@cred.club>
2024-08-29 13:54:25 +0530

    using 2 layers in the neural network

--- code/trigram_nn/model.py
+++ code/trigram_nn/model.py
@@ -23,7 +23,8 @@ class TrigramNN:
         self.targets: Tensor = None
 
         # the weights of the neural network
-        self.weights: Tensor = None
+        self.weights_1: Tensor = None
+        self.weights_2: Tensor = None
     
     def _make_char_int_mappings(self) -> None:
         letters: list = ['.'] + list(string.ascii_lowercase)
@@ -69,11 +70,30 @@ class TrigramNN:
         self.inputs = self.inputs.float()
         self.targets = self.targets.float()
     
+    def _init_nn(self) -> None:
+        """
+        Make the neural network weights.
+        """
+        l1_size: tuple[int, int] = (
+            self.inputs.shape[1], self.inputs.shape[1],
+        )
+        l2_size: tuple[int, int] = (
+            l1_size[1], self.targets.shape[1],
+        )
+
+        self.weights_1 = torch.randn(l1_size, requires_grad=True)
+        self.weights_2 = torch.randn(l2_size, requires_grad=True)
+    
     def _pred(self, inputs: Tensor) -> Tensor:
         """
         Find predictions based on the inputs passed and the weights trained.
         """
-        logits = inputs @ self.weights
+        # first layer
+        layer_output = F.relu(inputs @ self.weights_1)
+
+        # second layer
+        logits = layer_output @ self.weights_2
+
         probs: Tensor = F.softmax(logits, dim=1)
         return probs
 
@@ -82,12 +102,7 @@ class TrigramNN:
         This method trains the neural network.
         """
         self._make_training_data()
-
-        # init with random weights
-        size: tuple[int, int] = (
-            self.inputs.shape[1], self.targets.shape[1],
-        )
-        self.weights = torch.randn(size, requires_grad=True)
+        self._init_nn()
 
         for epoch in range(num_epochs):
             # forward pass
@@ -97,8 +112,11 @@ class TrigramNN:
             print(f'Epoch: {epoch}, Loss: {loss:.4f}')
 
             # backward pass
-            learning_rate: float = 50
-            self.weights.grad = None
+            learning_rate: float = 5
+            self.weights_1.grad = None
+            self.weights_2.grad = None
             loss.backward()
-            self.weights.data += (-learning_rate) * self.weights.grad 
+
+            self.weights_1.data += (-learning_rate) * self.weights_1.grad
+            self.weights_2.data += (-learning_rate) * self.weights_2.grad
         
