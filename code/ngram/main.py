7a12ec915ed8de15a918deb39cca7fd6b385b217
Siddharth Saha <siddharth.saha@cred.club>
2024-08-30 10:33:52 +0530

    starting work on the model

--- code/ngram/main.py
+++ code/ngram/main.py
@@ -3,13 +3,26 @@ The main file to start execution.
 """
 import argparse
 
+from ngram.model import NGramModel
+
+def _read_words(filepath: str) -> list:
+    words: list = []
+
+    with open(filepath, encoding='utf-8') as f:
+        words = f.read().splitlines()
+    
+    words = [w.lower().strip() for w in words]
+    return words
+
 
 def main(train_data_filepath: str, num_epochs: int) -> None:
     """
     The main method to start execution.
     """
-    print(f'{train_data_filepath=}')
-    print(f'{num_epochs=}')
+    words: list = _read_words(train_data_filepath)
+
+    model = NGramModel(words)
+    model.train(num_epochs)
 
 
 if __name__ == '__main__':
---
+++ code/ngram/model.py
@@ -0,0 +1,24 @@
+"""
+This contains the model implementation for the N-gram character model.
+"""
+
+import torch
+from torch import Tensor
+from torch.nn import functional as F
+import string
+
+
+class NGramModel:
+    """
+    This is the model implementation for an N-gram character model.
+    """
+    
+    def __init__(self, input_words: list) -> None:
+        self.input_words: list = input_words
+        print(f'Num of words: {len(self.input_words)}')
+    
+    def train(self, num_epcohs: int) -> None:
+        """
+        This method will train the model.
+        """
+        print(f'{num_epcohs=}')
