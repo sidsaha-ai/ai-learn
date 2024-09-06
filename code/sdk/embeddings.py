9ea352306fa86e26f09242f41f0604ccec37ac78
Siddharth Saha <siddharth.saha@cred.club>
2024-09-06 12:29:04 +0530

    enhance the embeddings class

--- code/sdk/embeddings.py
+++ code/sdk/embeddings.py
@@ -5,7 +5,11 @@ This class can be used to create embeddings for the inputs.
 import torch
 from torch import Tensor
 
+
 class Embedding:
+    """
+    Class to represent Embeddings.
+    """
 
     def __init__(
             self,
@@ -16,9 +20,32 @@ class Embedding:
         super().__init__()
 
         size: tuple[int, int] = (num_embeddings, embedding_dim)
-        self.embeddings: Tensor = torch.empty(
+        self.weights: Tensor = torch.empty(
             size, dtype=torch.float, requires_grad=True,
         )
 
-        # init with a uniform distribution
-        torch.nn.init.uniform_(self.embeddings, a=-0.1, b=0.1)
+        # init with a normal distribution with a mean of 0 and a standard deviation of 1.
+        torch.nn.init.normal_(self.weights, mean=0, std=1)
+
+    def __getitem__(self, index) -> Tensor:
+        return self.weights[index]
+    
+    def __matmul__(self, other: Tensor) -> Tensor:
+        """
+        Implements the @ operator like `embedding @ other`.
+        """
+        return self.weights @ other
+    
+    def __rmatmul__(self, other: Tensor) -> Tensor:
+        """
+        Implements the @ operator like `other @ embedding`.
+        """
+        return other @ self.weights
+    
+    @property    
+    def shape(self) -> tuple[int, int]:
+        return self.weights.shape
+    
+    def view(self, size: tuple[int, int]) -> Tensor:
+        return self.weights.view(size)
+    
