a5b8e82e7eccfab4c2e33799f2bff56ef2a2726a
Siddharth Saha <siddharth.saha@cred.club>
2024-09-06 16:12:12 +0530

    fix lint

--- code/sdk/tests/test_tanh.py
+++ code/sdk/tests/test_tanh.py
@@ -22,10 +22,10 @@ def test_call(num_rows: int, num_cols: int) -> None:
     """
     size = (num_rows, num_cols)
     x: Tensor = torch.randn(size, dtype=torch.float)
-    
+
     tanh = Tanh()
     y = tanh(x)
-    
+
     assert y is not None
     assert y.shape[0] == num_rows
     assert y.shape[1] == num_cols
