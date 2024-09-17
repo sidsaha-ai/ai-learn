ed3d71a24e4a387a997ed227d82af507e2a38917
Siddharth Saha <siddharth.saha@cred.club>
2024-09-17 11:02:25 +0530

    learning rate scheduling

--- code/novels_generator/experiments/training/expt_6/details.md
+++ code/novels_generator/experiments/training/expt_6/details.md
@@ -5,3 +5,9 @@ From the previous experiments, we saw very small improvements. In this experimen
 Let's increase the model complexity so that the model learns more complex patterns in the data. To do this, let's tune the following hyperparameters - 
 - **Increase the number of transformer layers** from `4` to `8` so that we add depth to capture more complex patterns.
 - **Increase the feed-forward size** to `4096`, so that the model again learns more deeply.
+
+#### Learning Rate scheduling
+Let's also experiment with learning rate scheduling. Let's do the following things - 
+- Like previous experiments, we will start warming up the learning rate from 1e-5 for the first 5 epochs, but with **cosine annealing**.
+- Then, we will increase the learning rate to 5e-4 (from previously of 1e-4) so that the model can converge a little faster than before.
+- Then, we will use **cosine annealing** to decay the learning rate more smoothly rather than abruptly changing it.
--- code/novels_generator/experiments/training/expt_6/run.py
+++ code/novels_generator/experiments/training/expt_6/run.py
@@ -2,15 +2,16 @@
 This script runs the experiment.
 """
 
+import math
+
 from novels_generator.code import train
 from novels_generator.code.constants import Hyperparamters
+
 
 def main():
     """
     The main function to run this experiment.
     """
-
-    num_epochs: int = 30
 
     # hyperparameters
     Hyperparamters.CONTEXT_LENGTH = 256
@@ -21,15 +22,34 @@ def main():
     Hyperparamters.NUM_LAYERS = 8
     Hyperparamters.FEED_FORWARD_SIZE = 4096
 
-    def lr_lambda(epoch):
-        if epoch < 5:
+    warmup_epochs: int = 5
+    constant_lr_epochs: int = 5
+    total_num_epochs: int = 30
+
+    def lr_schedule(epoch):
+        current_epoch: int = epoch + 1
+
+        if current_epoch <= warmup_epochs:
+            # learning rate should be 1e-5
             return 1e-1
-        if epoch < 25:
-            return 1
-        return 1e-2
+        
+        if warmup_epochs < current_epoch <= (warmup_epochs + constant_lr_epochs):
+            # learning rate should be 5e-4
+            return 5
+        
+        if epoch > constant_lr_epochs:
+            # learning rate should be cosine annealing to 1e-6 from 5e-4
+            num_cosine_epochs: int = total_num_epochs - warmup_epochs - constant_lr_epochs
+            num_elapsed_epochs: int = current_epoch - warmup_epochs - constant_lr_epochs
+            min_lr = 1e-6
+            max_lr = 5e-4
+            initial_lr = 1e-5
+
+            decay = 0.5 * (1 + math.cos(math.pi * num_elapsed_epochs / num_cosine_epochs))
+            return (min_lr + (max_lr - min_lr) * decay) / initial_lr
     
     train.train_model(
-        num_epochs, lr_scheduler_type='LambdaLR', lr_lambda=lr_lambda,
+        total_num_epochs, lr_scheduler_type='LambdaLR', lr_lambda=lr_schedule,
     )
 
 if __name__ == '__main__':
