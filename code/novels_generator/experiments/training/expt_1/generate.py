cdbc20842c3cec4893f02aa7f5559ca3cb494a8f
Siddharth Saha <siddharth.saha@cred.club>
2024-09-18 20:25:04 +0530

    remove not needed functions

--- code/novels_generator/experiments/training/expt_1/generate.py
+++ code/novels_generator/experiments/training/expt_1/generate.py
@@ -4,42 +4,8 @@ The file generates the book from the model of this experiment.
 
 import os
 
-import torch
-from novels_generator.code.model import BooksTransformerModel
 from novels_generator.experiments.training.expt_1 import hyperparameters
 from novels_generator.experiments.training.generation_utils import GenUtils
-
-
-def load_model() -> BooksTransformerModel:
-    """
-    Load the model from the path.
-    """
-    path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'model.pth',
-    )
-
-    print(f'Loading model from {path}...')
-    model = BooksTransformerModel()
-    model.load_state_dict(torch.load(path))
-
-    # set the model to evaluation mode
-    model.eval()
-    print(f'Model loaded from path {path}.')
-
-    return model
-
-
-def write_to_file(text: str) -> None:
-    """
-    Write the generated text to file.
-    """
-    path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'generated_text.txt',
-    )
-
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(text)
-    print(f'Wrote {len(text)} characters to file {path}')
 
 
 def main():
