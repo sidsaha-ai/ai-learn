a7550d40bf3d7d9ea9d963ba8e93aa689e5d3ed9
Siddharth Saha <siddharth.saha@cred.club>
2024-09-26 11:33:30 +0530

    implement attention module for batches

--- code/llm_book/attention.py
+++ code/llm_book/attention.py
@@ -20,17 +20,17 @@ class Attention(torch.nn.Module):
         self.w_value = torch.nn.Linear(in_dim, out_dim, bias=False)
         self.dropout = torch.nn.Dropout(dropout_percent)
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+    def forward(self, batch: torch.Tensor) -> torch.Tensor:
         """
-        Applies the attention mechanism.
+        Applies the forward pass to a batch of inputs.
         """
-        query = self.w_query(inputs)
-        key = self.w_key(inputs)
-        value = self.w_value(inputs)
+        query = self.w_query(batch)  # 4x6x2
+        key = self.w_key(batch)      # 4x6x2
+        value = self.w_value(batch)  # 4x6x2
 
-        attn_score = query @ key.T
+        attn_score = query @ key.transpose(1, 2)
 
-        # apply masking to attention scores
+        # apply masking
         attn_score = attn_score.masked_fill(
             ~torch.tril(torch.ones_like(attn_score)).bool(), -torch.inf,
         )
@@ -39,37 +39,31 @@ class Attention(torch.nn.Module):
         attn_weights = torch.nn.functional.softmax(
             attn_score / math.sqrt(key.shape[-1]), dim=-1,
         )
-        # apply dropout to weights
-        attn_weights = self.dropout(attn_weights)
+        # apply dropout
+        attn_weights = self.dropout(attn_weights)  # 4x6x6
+        print(attn_weights.shape)
 
-        outputs = attn_weights @ value
-
+        outputs = attn_weights @ value  # 4x6x2
         return outputs
-
 
 def main():
     """
     The main method to test the attention module.
     """
+    size = (6, 3)
     inputs = [
-        [0.43, 0.15, 0.89],  # your
-        [0.55, 0.87, 0.66],  # journey
-        [0.57, 0.85, 0.64],  # starts
-        [0.22, 0.58, 0.33],  # with
-        [0.77, 0.25, 0.10],  # one
-        [0.05, 0.80, 0.55],  # step
+        torch.randn(size), torch.randn(size), torch.randn(size), torch.randn(size),
     ]
+    batch = torch.stack([sample for sample in inputs])
 
-    inputs = torch.tensor(inputs)
-
-    in_dim: int = inputs.shape[1]
+    in_dim: int = batch.shape[-1]
     out_dim: int = 2
     dropout_percent: float = 0.2
 
-    attention = Attention(in_dim, out_dim, dropout_percent)
-
-    res = attention(inputs)
+    attn = Attention(in_dim, out_dim, dropout_percent)
+    res = attn(batch)
     print(res)
+
 
 if __name__ == '__main__':
     main()
