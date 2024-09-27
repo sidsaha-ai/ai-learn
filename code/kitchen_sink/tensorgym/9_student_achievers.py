7972c2f78f38f4cbf348a563915aac3d95615e3f
Siddharth Saha <siddharth.saha@cred.club>
2024-09-27 09:38:31 +0530

    fix isort

--- code/kitchen_sink/tensorgym/9_student_achievers.py
+++ code/kitchen_sink/tensorgym/9_student_achievers.py
@@ -3,6 +3,7 @@ This contains the solution for the exercise https://tensorgym.com/exercises/6
 """
 import torch
 
+
 def solution(scores: torch.Tensor) -> torch.Tensor:
     """
     This implements the solution to the exercise.
